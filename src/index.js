9be0ead7283e1ccd9f2019432a8a10354339d093
CSE-pebble <CSE.pebble@gmail.com>
2023-11-03 22:14:27 +0900

    Add text changing

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-const dropDown_clickable = document.querySelector(".dropDown_clickable");
+const dropDownClickable = document.querySelector(".dropDown_clickable");
+const dropDownSelected = document.querySelector(".dropDown_clickable span");
 const dropDownContent = document.querySelector(".dropDown_content");
 const dropDownContents = document.querySelectorAll(".dropDown_content a");
 
 let state = "off";
-function handleClick() {
+function handleClikableClick() {
   if (state === "off") {
     dropDownContent.style.display = "flex";
     state = "on";
@@ -18,12 +19,23 @@ function handleMouseenter(event) {
   selected.style.backgroundColor = "lightgrey";
 }
 
-function handleMouseleave() {
+function handleMouseleave(event) {
   let selected = event.currentTarget;
   selected.style.backgroundColor = "whitesmoke";
 }
 
-dropDown_clickable.addEventListener("click", handleClick);
+let clicked = dropDownContents[1];
+function handleContentClick(event) {
+  console.log(clicked);
+  clicked.classList.remove("dropDown_content_clicked");
+  clicked = event.currentTarget;
+  clicked.classList.add("dropDown_content_clicked");
+  dropDownSelected.innerText = clicked.innerText;
+  dropDownContent.style.display = "none";
+  state = "off";
+}
+
+dropDownClickable.addEventListener("click", handleClikableClick);
 
 dropDownContents.forEach((content) => {
   content.addEventListener("mouseenter", handleMouseenter);
@@ -31,4 +43,8 @@ dropDownContents.forEach((content) => {
 
 dropDownContents.forEach((content) => {
   content.addEventListener("mouseleave", handleMouseleave);
+});
+
+dropDownContents.forEach((content) => {
+  content.addEventListener("click", handleContentClick);
 });
