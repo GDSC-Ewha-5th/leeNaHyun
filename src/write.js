61317760a6f134d779430b4e38381c37b9ca7a5c
CSE-pebble <CSE.pebble@gmail.com>
2023-11-03 22:36:45 +0900

    Add filterting duplication

--- src/write.js
+++ src/write.js
@@ -1,10 +1,29 @@
 const input = document.querySelector(".input");
 
+let tagArr = [];
+
+function isExist(value) {
+  for (let i = 0; i < tagArr.length; i++) {
+    if (value == tagArr[i]) return true;
+  }
+  return false;
+}
+
+function deleteTag(event) {
+  event.currentTarget.par;
+}
+
 function addTag(event) {
   if (event.key === "Enter") {
-    const newTag = document.createElement("span");
-    newTag.innerText = event.target.value;
-    document.body.insertBefore(newTag, input);
+    if (!isExist(event.target.value)) {
+      const newTag = document.createElement("span");
+      newTag.innerText = event.target.value;
+      document.body.insertBefore(newTag, input);
+      tagArr.push(event.target.value);
+      newTag.addEventListener("click", () => {
+        newTag.remove();
+      });
+    }
     event.target.value = "";
   }
 }
