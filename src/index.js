83afcff73a584b61e63c15bef5018171eba02397
CSE-pebble <CSE.pebble@gmail.com>
2023-11-03 21:57:03 +0900

    Modify mouse event

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 const dropDown_clickable = document.querySelector(".dropDown_clickable");
 const dropDownContent = document.querySelector(".dropDown_content");
-const eachDropDownContent = document.querySelectorAll(".dropDown_content span");
+const dropDownContents = document.querySelectorAll(".dropDown_content a");
 
 let state = "off";
-
 function handleClick() {
   if (state === "off") {
     dropDownContent.style.display = "flex";
@@ -14,52 +13,22 @@ function handleClick() {
   }
 }
 
-function findIndex() {
-  alert("hi");
+function handleMouseenter(event) {
+  let selected = event.currentTarget;
+  selected.style.backgroundColor = "lightgrey";
 }
 
-function handleMouseenter(index) {
-  if (state === "on") {
-    const isMouseOnContent = document.querySelector(
-      `.dropDown_content span:nth-child(${index + 1})`
-    );
-    isMouseOnContent.style.backgroundColor = "lightgrey";
-  }
-}
-
-function handleMouseleave(index) {
-  if (state === "on") {
-    const isMouseLeaveContent = document.querySelector(
-      `.dropDown_content span:nth-child(${index + 1})`
-    );
-    isMouseLeaveContent.style.backgroundColor = "whitesmoke";
-  }
+function handleMouseleave() {
+  let selected = event.currentTarget;
+  selected.style.backgroundColor = "whitesmoke";
 }
 
 dropDown_clickable.addEventListener("click", handleClick);
 
-eachDropDownContent[0].addEventListener("mouseenter", function () {
-  handleMouseenter(0);
-});
-eachDropDownContent[1].addEventListener("mouseenter", function () {
-  handleMouseenter(1);
-});
-eachDropDownContent[2].addEventListener("mouseenter", function () {
-  handleMouseenter(2);
-});
-eachDropDownContent[3].addEventListener("mouseenter", function () {
-  handleMouseenter(3);
+dropDownContents.forEach((content) => {
+  content.addEventListener("mouseenter", handleMouseenter);
 });
 
-eachDropDownContent[0].addEventListener("mouseleave", function () {
-  handleMouseleave(0);
-});
-eachDropDownContent[1].addEventListener("mouseleave", function () {
-  handleMouseleave(1);
-});
-eachDropDownContent[2].addEventListener("mouseleave", function () {
-  handleMouseleave(2);
-});
-eachDropDownContent[3].addEventListener("mouseleave", function () {
-  handleMouseleave(3);
+dropDownContents.forEach((content) => {
+  content.addEventListener("mouseleave", handleMouseleave);
 });
