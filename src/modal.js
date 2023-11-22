c526d3172727300bfb21170f9df8a39ad10567e1
CSE-pebble <CSE.pebble@gmail.com>
2023-11-22 18:40:06 +0900

    Add modal close

--- src/modal.js
+++ src/modal.js
@@ -2,12 +2,27 @@ export function createModal() {
   const card = document.querySelectorAll(".card");
   for (let i = 0; i < card.length; i++) {
     card[i].addEventListener("click", () => {
-      const modal = document.createElement("div");
-      document.body.appendChild(modal);
-      modal.setAttribute("class", "modal_background");
+      const modalBg = document.createElement("div");
+      document.body.appendChild(modalBg);
+      modalBg.setAttribute("class", "modal_background");
 
       const body = document.querySelector("body");
-      body.className += "scroll_hidden";
+      body.classList.add("scroll_hidden");
+
+      const modal = document.createElement("div");
+      modalBg.appendChild(modal);
+      modal.setAttribute("class", "card modal");
+
+      const closeBtn = document.createElement("span");
+      modalBg.appendChild(closeBtn);
+      closeBtn.setAttribute("class", "modal_closeBtn");
+      closeBtn.innerText = "X";
+      closeBtn.addEventListener("click", () => {
+        body.classList.remove("scroll_hidden");
+        modalBg.remove();
+        modal.remove();
+        closeBtn.remove();
+      });
     });
   }
 }
