3738c9423fd3a25771c67a14601a8bfe8e854af6
CSE-pebble <CSE.pebble@gmail.com>
2023-11-25 23:34:16 +0900

    [Not Solved] DropDown

--- React/src/component/ShowDropDown.jsx
+++ React/src/component/ShowDropDown.jsx
@@ -1,21 +1,11 @@
 import styled from "styled-components";
 
-const Block = styled.div`
+const DropDownBlock = styled.div`
   display: flex;
   flex-direction: column;
   position: absolute;
   z-index: 1;
   margin-top: 10px;
-
-  a {
-    background-color: whitesmoke;
-    font-size: 20px;
-    padding: 15px;
-    border-bottom: 1px solid lightgrey;
-    width: 200px;
-    height: 20px;
-    align-items: center;
-  }
 
   .first {
     border-top-left-radius: 5px;
@@ -28,21 +18,34 @@ const Block = styled.div`
   }
 `;
 
+const DropDownContents = styled.a`
+  background-color: whitesmoke;
+  font-size: 20px;
+  padding: 15px;
+  border-bottom: 1px solid lightgrey;
+  width: 200px;
+  height: 20px;
+  align-items: center;
+  &:hover {
+    color: purple;
+  }
+`;
+
 function ShowDropDown() {
   return (
     <>
-      <Block>
-        <a className="first" href="#">
+      <DropDownBlock>
+        <DropDownContents className="first" href="#">
           오늘
-        </a>
-        <a className="dropDown_content_clicked" href="#">
+        </DropDownContents>
+        <DropDownContents className="dropDown_content_clicked" href="#">
           이번 주
-        </a>
-        <a href="#">이번 달</a>
-        <a className="last" href="#">
+        </DropDownContents>
+        <DropDownContents href="#">이번 달</DropDownContents>
+        <DropDownContents className="last" href="#">
           올해
-        </a>
-      </Block>
+        </DropDownContents>
+      </DropDownBlock>
     </>
   );
 }
