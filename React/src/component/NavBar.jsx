ffa258349c64fa169bcad0ed0d2d027f8995b537
CSE-pebble <CSE.pebble@gmail.com>
2023-11-25 18:11:49 +0900

    [Temp] Add NavBar.jsx CSS

--- React/src/component/NavBar.jsx
+++ React/src/component/NavBar.jsx
@@ -3,43 +3,51 @@ import styled from "styled-components";
 function NavBar() {
   return (
     <>
-      <div>
-        <div>
-          <div>
-            <nav>
-              <Icon className="material-symbols-outlined"> show_chart </Icon>
-              <a href="#">트렌딩</a>
-            </nav>
-            <nav>
-              <Icon className="material-symbols-outlined"> schedule </Icon>
-              <a href="#">최신</a>
-            </nav>
-          </div>
-          <div>
-            <div>
-              <span>이번 주</span>
-              <span className="material-symbols-outlined">
-                {" "}
-                arrow_drop_down{" "}
-              </span>
-            </div>
-            <div>
-              <a href="#">오늘</a>
-              <a className="dropDown_content_clicked" href="#">
-                이번 주
-              </a>
-              <a href="#">이번 달</a>
-              <a href="#">올해</a>
-            </div>
-          </div>
-        </div>
-        <div>
-          <span className="material-symbols-outlined"> more_vert </span>
-        </div>
-      </div>
+      <Container>
+        <Box1>
+          <Icon className="material-symbols-outlined"> show_chart </Icon>
+          <Text>트렌딩</Text>
+
+          <Icon className="material-symbols-outlined"> schedule </Icon>
+          <Text>최신</Text>
+
+          <Text>이번 주</Text>
+          <Icon className="material-symbols-outlined"> arrow_drop_down </Icon>
+          <DropDown>
+            <Text>오늘</Text>
+            <Text className="dropDown_content_clicked" href="#">
+              이번 주
+            </Text>
+            <Text href="#">이번 달</Text>
+            <Text href="#">올해</Text>
+          </DropDown>
+        </Box1>
+        <Box2>
+          <Icon className="material-symbols-outlined"> more_vert </Icon>
+        </Box2>
+      </Container>
     </>
   );
 }
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 30px 10px;
+`;
+
+const Box1 = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Text = styled.span`
+  font-size: 25px;
+`;
+
+const DropDown = styled.span``;
+
+const Box2 = styled.div``;
 
 const Icon = styled.span`
   font-size: 35px;
